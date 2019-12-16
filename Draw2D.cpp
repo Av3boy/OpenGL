82bcd567ad4a748c9c5414caffc31ecfbe5873b0
Veikkolainen <aveboy.00@gmail.com>
2019-12-16 04:35:23 +0200

    Add 2D drawing

---
+++ Draw2D.cpp
@@ -0,0 +1,18 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "Draw2D.h"
+
+/*void Draw2D::DrawTriangle()
+{
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-0.5f, -0.5f, 0.0f);
+	glColor3f(0, 0, 0);
+
+	glVertex3f(0.0f, 0.5f, 0.0f);
+	glColor3f(255, 255, 255);
+
+	glVertex3f(0.5f, -0.5f, 0.0f);
+	glColor3f(122, 122, 122);
+	glEnd();
+}*/
---
+++ Draw2D.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class Draw2D
+{
+public:
+	Draw2D();
+	~Draw2D();
+
+	//static void DrawTriangle();
+
+private:
+
+};
+
+Draw2D::Draw2D()
+{
+}
+
+Draw2D::~Draw2D()
+{
+}
